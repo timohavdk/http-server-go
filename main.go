521ebdfd86f2b340aa74197f2a7f1424444022ef
timohavdk <timoha200120@gmail.com>
2023-03-26 15:08:40 +1000

    #5 Using httprouter for multiplexer

--- main.go
+++ main.go
@@ -2,27 +2,21 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"reflect"
-	"runtime"
 )
 
-func name(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Timofey")
-}
-
-func log(h http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-		fmt.Println("Handler function called - " + name)
-		h(w, r)
-	}
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
 }
 
 func main() {
+	mux := httprouter.New()
+	mux.GET("/hello/:name", hello)
+
 	server := http.Server{
-		Addr: "127.0.0.1:8080",
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
 	}
-	http.HandleFunc("/name", log(name))
 	server.ListenAndServe()
 }
