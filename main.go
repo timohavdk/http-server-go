2d17fa80e943d76fb90ae5be7411daa4ade4bdb1
timohavdk <timoha200120@gmail.com>
2023-03-26 14:23:50 +1000

    #3 Handle function for server

--- main.go
+++ main.go
@@ -5,27 +5,19 @@ import (
 	"net/http"
 )
 
-type Index struct {
-}
-
-func (h *Index) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Index page")
 }
 
-type About struct {
-}
-
-func (h *About) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func about(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "About page")
 }
 
 func main() {
-	index := Index{}
-	about := About{}
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.Handle("/", &index)
-	http.Handle("/about/", &about)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
 	server.ListenAndServe()
 }
